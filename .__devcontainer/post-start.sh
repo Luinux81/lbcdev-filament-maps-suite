3a6ede027308c4a125dcfc3e994fecd332d4b387
luinux81 <luinux81@gmail.com>
2026-02-08 03:02:20 +0000

    Limpieza

--- .__devcontainer/post-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "🔄 Post-start commands..."
-
-# Actualizar submodules (por si hay cambios remotos)
-git submodule update --remote --merge
-
-echo "✅ Container iniciado correctamente"
